46448ac9f2ebbf71ec07c1a1d6d6722fb6eaf72c
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2017-05-24 11:54:24 -0300

    Update autoload.php

--- public_html/app/config/autoload.php
+++ public_html/app/config/autoload.php
@@ -6,7 +6,7 @@ function auto($Class) {
     $cDir = array('Models','Controllers','Helpers','Conn');
     $iDir = null;
     foreach ($cDir as $dirName):
-        if(!$iDir && file_exists($_SERVER["DOCUMENT_ROOT"]."/app/{$dirName}/{$Class}.php") && !is_dir($_SERVER["DOCUMENT_ROOT"]."/app/{$dirName}/{$Class}.php")){
+        if(!$iDir && file_exists(__DIR__."/../{$dirName}/{$Class}.php") && !is_dir($_SERVER["DOCUMENT_ROOT"]."/app/{$dirName}/{$Class}.php")){
             include_once ($_SERVER["DOCUMENT_ROOT"]."/app/{$dirName}/{$Class}.php");
             $iDir = true;
         }
